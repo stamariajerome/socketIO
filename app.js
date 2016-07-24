ff805f0e81b7f2b6cd4110a39611f00af4f8f181
Jerome Sta. Maria <stamaria.jerome@gmail.com>
2016-07-25 03:01:17 +0800

    heroku IP and PORT added

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var http = require('http').Server(app);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-http.listen('3000', function() {
+http.listen(process.env.PORT,process.env.IP, function() {
   console.log('server up!');
 });
